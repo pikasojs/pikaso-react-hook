e49bd480c82a6d03e2e1556a39fb654fbeb733b4
Ramin <ramin.mousavy@gmail.com>
2021-10-09 08:48:38 +0300

    feat: upgrade to v2.0.0

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
 import { useRef, useState, useEffect, RefObject } from 'react'
 
-import Pikaso, { Settings } from 'pikaso'
+import Pikaso, { BaseShapes, RegisterShapesFn, Settings } from 'pikaso'
 
-export default function usePikaso(
-  options: Partial<Settings> = {}
-): [RefObject<HTMLDivElement>, Pikaso | null] {
-  const [instance, setInstance] = useState<Pikaso | null>(null)
+export default function usePikaso<T extends BaseShapes = BaseShapes>(
+  options: Partial<Settings> = {},
+  registerShapes?: RegisterShapesFn<T>
+): [RefObject<HTMLDivElement>, Pikaso<T> | null] {
+  const [instance, setInstance] = useState<Pikaso<T> | null>(null)
   const ref = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
-    const editor = new Pikaso({
-      container: ref.current as HTMLDivElement,
-      ...options
-    })
+    const editor = new Pikaso<T>(
+      {
+        container: ref.current as HTMLDivElement,
+        ...options
+      },
+      registerShapes
+    )
 
     setInstance(editor)
   }, [])
